47f721fa40e3db453e6f895310bb9acbb882795b
Akash Mane <akash21mane21@gmail.com>
2022-03-22 14:30:14 +0530

    footer changed

--- comps/Footer.js
+++ comps/Footer.js
@@ -4,8 +4,8 @@ import styles from '../styles/Home.module.scss'
 export default function Footer() {
   return (
   <div className={styles.Footer}>
-    <h1>Developed by InnovArithm</h1>
-    <h3><a href="https://akash-mane.netlify.app/">Akash Mane</a> </h3>
+    <h1>Developed by Akash Mane</h1>
+    <h3><a href="https://akash-mane.netlify.app/"></a> </h3>
   </div>
   );
 }
